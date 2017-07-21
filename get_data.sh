fdadf547cb854a823ebc412af81fed351a0efccc
gylee <gylee1103@gmail.com>
2017-07-21 11:07:29 +0000

    delete old version

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-wget 'https://www.dropbox.com/s/1w49tubj9aap4tc/SketchDB.zip?dl=1' -O SketchDB.zip
+wget 'https://www.dropbox.com/s/nl2kudv02uye72h/SketchDB.zip?dl=1' -O SketchDB.zip
 unzip SketchDB.zip
 rm SketchDB.zip
-rm -rf __MACOSX
-
